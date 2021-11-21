e8a4a5c2b489c548ad3516e50f630e055b13fbcf
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-11-21 17:22:40 +0530

    Implemented forgot password feature

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel');
 const AsyncErrorHandler = require('../middlewares/asyncErrorHandler');
 const ErrorHandler = require('../utils/errorHandler');
 const sendToken = require('../utils/jwtToken');
+const sendEmail = require('../utils/sendEmail');
 
 // register a new user
 const registerUser = AsyncErrorHandler(async (req, res, next) => {
@@ -46,8 +47,47 @@ const logOutUser = AsyncErrorHandler(async (req, res, next) => {
     });
 })
 
+// forgot password
+const forgotPassword = AsyncErrorHandler(async (req, res, next) => {
+    const user = await User.findOne({email: req.body.email});
+
+    if (!user) {
+        return next(new ErrorHandler('User doesn\'t exist', 404));
+    }
+
+    // generating reset password token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    const resetPasswordUrl = `${req.protocol}://${req.get('host')}/api/v1/user/reset/${resetToken}`;
+
+    const message = `Your password reset token is:\n\n${resetPasswordUrl}.\n\nIf you have not requested for this email, please ignore this.`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'ServiceFare password recovery',
+            message
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `E-mail sent to ${user.email} successfully`
+        });
+    } catch (err) {
+        user.resetPasswordToken = undefined;
+        user.expirePasswordToken = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(err.message, 500));
+    }
+})
+
 module.exports = {
     registerUser,
     logInUser,
-    logOutUser
+    logOutUser,
+    forgotPassword
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { registerUser, logInUser, logOutUser } = require('../controllers/userController');
+const { registerUser, logInUser, logOutUser, forgotPassword } = require('../controllers/userController');
 const { isAuthenticatedUser } = require('../middlewares/auth');
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(logInUser);
 router.route('/logout').get(isAuthenticatedUser, logOutUser);
+router.route('/forgotpassword').post(forgotPassword);
 
 module.exports = router;
