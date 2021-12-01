0c040691fbe9d1be8704748a4121f039a13d34c8
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-12-01 11:22:43 +0530

    Added  cloudinary for uploading images

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
@@ -8,6 +9,7 @@ const errorMiddlerware = require('./middlewares/error');
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(fileUpload());
 
 // setting up routes
 const user = require('./routes/userRoute');
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config({ path: 'backend/config/config.env' });
 const app = require('./app');
+const cloudinary = require('cloudinary');
+
 const connectDB = require('./config/database');
 
 // handling uncaught errors
@@ -11,6 +13,13 @@ process.on('uncaughtException', (err) => {
 
 // connect database
 connectDB();
+
+// cloudinary setup
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
