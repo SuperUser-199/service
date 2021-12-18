4d09e8491be46be1982dab58dcdfb3c5345b440c
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-12-18 13:48:44 +0530

    DB changed

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const res = await mongoose.connect(process.env.LOCAL_DB);
+    const res = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`Database connected successfully to ${res.connection.host}`);
 }
 
